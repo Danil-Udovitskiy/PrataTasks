cf38669e26c59b4bffebfee7aa82cda72317f733
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-11-21 11:24:24 +0200

    using job code 5 where the 1 minute wait condition is already implemented
    1- added check for overflow of two queues
    2- I added a boolean indicator to check if the queues are equal or one less than the other
    3 - added check of waiting time more than mine and empty queue
    4- added a boolean indicator to check dequeueing if the queues are equal or one less than the other
    5- added size increase for the second queue

--- Chapter 12 Task 6/Chapter 12 Task 6/Chapter 12 Task 6.cpp
+++ Chapter 12 Task 6/Chapter 12 Task 6/Chapter 12 Task 6.cpp
@@ -29,11 +29,11 @@ int main()
 
 	int qs;
 	cin >> qs;
-	Queue line(qs); // queue can contain up to qs people
+	Queue first_line(qs); // queue can contain up to qs people
 
 	//adding second queue 
 	cout << "Enter maximum size of second queue: "; // enter the maximum second queue size
-	int qs2; //++
+	int qs2;
 	cin >> qs2;
 	Queue second_line(qs2); // second queue can contain up to qs people
 
@@ -59,61 +59,114 @@ int main()
 
 
 	//Add a loop with a 1 minute wait condition
+
 	while (avarage_wait_time < 1) // condition for a total wait time of 1 minute
 	{
 		min_per_cust = MIN_PER_HR / perhour; //initialize variable to generate client spawn
 		//first return from the function newcustomer = false (default) , that is the client does not appear
 
+		
+		//I add a variable indicator to which queue to add
+		bool is_to_first = false;
+
+		//I add a variable indicator from which queue to read
+		bool is_from_first = false;
+
+
 		// Start Simulation
 		for (int cycle = 0; cycle < cyclelimit; cycle++)
 		{
+
+			//inside the loop, when a new client arrives, we check which of the queues has fewer people
+			//if the first is less than the second, then put it in the first
+			//otherwise in the second
+			
+
+
 			if (newcustomer(min_per_cust)) // client approached
 			{
-				if (line.isfull() && second_line.isfull()) // ++ && second_line.isfull() проверяем не заполнены ли обе очереди 
+				if (first_line.isfull() && second_line.isfull()) // ++ && second_line.isfull() - check if both queues are full
 					turnaways++;
 				else
 				{
+					//I put out the identical code before the if so that there are no repetitions
 					customers++;
 					temp.set(cycle); // cycle = arrival time
 					
-					//++
-					if (qs <= qs2) //если первая очередь <= второй, то добавляем нового клиента в очередь 1
-					line.enqueue(temp); // adding a newcomer to the queue
+					//then check if the first queue is less than the second
+					//+ boolean variable, if the queues are equal, selects where to add the client (if the last addition was in the first place, then the indicator takes the value of true and goes to elc - adding a new client to the second queue and vice versa)
+					if ((first_line.queuecount() < second_line.queuecount()) || (first_line.queuecount() == second_line.queuecount() && is_to_first == false))
+					
+					//if the first queue < the second, then add a new client to queue 1 (and the last addition was to the second one) 
 
-					else if (qs2 < qs) //если вторая очередь < первой, то добавляем нового клиента в очередь 2
-					second_line.enqueue(temp);
+					{
+						first_line.enqueue(temp); // adding a newcomer to the queue
+						is_to_first = true; //indicator of adding a client from the queue
+					}
+
+					else  //if the second queue is < the first, then add a new client to queue 2
+					{
+						second_line.enqueue(temp);
+						is_to_first = false; //indicator of adding a client from the queue
+					}
 				}
 			}
 
 
-			if (wait_time <= 0 && !line.isempty())
+
+
+			
+
+
+
+			//when we read (remove from the queue), we need an indicator
+			
+			if (wait_time <= 0 && (!first_line.isempty() || !second_line.isempty()))
 			{
-				line.dequeue(temp); // serving the next client
+
+				//I put out the identical code before the if so that there are no repetitions
+
 				wait_time = temp.ptime(); // during wait_time minutes
 				line_wait += cycle - temp.when();
 				served++;
-			}
 
-			//++
-			if (wait_time <= 0 && !second_line.isempty()) //если очередь 2 не пустая и время ожидания <= 0, то переходим к следующему клиенту
-			{
-				second_line.dequeue(temp); // serving the next client
-				wait_time = temp.ptime(); // during wait_time minutes
-				line_wait += cycle - temp.when();
-				served++;
-			}
 
+				//this is the reverse logic, from the first queue, deletion occurs first of all from queue 1 (respectively, the > sign)
+				//then check if the first queue is greater than the second
+				//+ boolean variable if the queues are equal selects where to remove the client from (if the last removal was from the first queue, then the indicator takes the value of true and goes to elc - removing the client from the second queue and vice versa)
+
+				if ((first_line.queuecount() > second_line.queuecount()) || (first_line.queuecount() == second_line.queuecount() && is_from_first == false))
+				{
+					first_line.dequeue(temp); // serving the next client
+
+					is_from_first = true; //indicator of removing the client from the queue
+				}
+
+				else
+				{
+					second_line.dequeue(temp); // serving the next client
+
+					is_from_first = false; //indicator of removing the client from the queue
+				}
+			}
+			
+
+			
 			if (wait_time > 0)
 				wait_time--;
 
-			sum_line += line.queuecount();
+
+			sum_line += first_line.queuecount(); //increase total queue size 1
+			sum_line += second_line.queuecount(); //increase total queue size 2
 
 		}
 
 		avarage_wait_time = (double)line_wait / served; //divide the total waiting time by serviced during emulation
 		perhour++; // increase the number of clients per hour (increments until the wait time is 1 minute)
-
 	}
+
+
+
 
 
 	//Added the condition for checking the presence of clients
@@ -132,9 +185,8 @@ int main()
 	}
 	else
 	{
-		cout << "No customers! \n"; // клиентов нет  
+		cout << "No customers! \n";
 	}
-
 
 	cout << "\nDone!\n";
 	return 0;
