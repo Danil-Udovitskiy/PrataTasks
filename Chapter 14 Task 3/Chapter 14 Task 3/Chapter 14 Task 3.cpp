453faf090d7cf7d9ea59b2f8a3c38a235616d5a6
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-12 23:25:47 +0200

    Moved the definition of Queue methods to Worker.h, to correctly use the template and create a queue of pointers to Worker Еhe error was that I just created an array of pointers to the Worker, without using the Queue template

--- Chapter 14 Task 3/Chapter 14 Task 3/Chapter 14 Task 3.cpp
+++ Chapter 14 Task 3/Chapter 14 Task 3/Chapter 14 Task 3.cpp
@@ -14,51 +14,12 @@ int main()
 	using std::cout;
 	using std::endl;
 	using std::strchr;
-	Worker* lolas[SIZE]; //created a queue of pointers to the Worker
 
+	//the error was that I just created an array of pointers to the Worker, 
+	//without using the Queue template
 
-	//testing the creation of the Waiter object from the ABS Worker
-	int ct;
-	for (ct = 0; ct < SIZE; ct++)
-	{
-		char choice;
-		cout << "Enter the employee category:\n" // enter the employee category;
-			<< "w: waiter  s: singer "
-			<< " t: singing waiter (q - exit) ";
-		cin >> choice;
-		while (strchr("wstq", choice) == NULL)
-		{
-			cout << "Please enter w / s / t / (q - exit) : ";
-			cin >> choice;
-		}
-		if (choice == 'q')
-			break;
-		switch (choice)
-		{
-		case 'w': lolas[ct] = new Waiter;
-			break;
-
-		case 's': lolas[ct] = new Singer;
-			break;
-
-		case 't': lolas[ct] = new SingingWaiter;
-			break;
-		}
-		cin.get();
-		lolas[ct]->Set();
-	}
-	cout << "\nHere is your staff:\n"; // output the list of employees
-	int i;
-	for (i = 0; i < ct; i++)
-	{
-		cout << endl;
-		lolas[i]->Show();
-	}
-	for (i = 0; i < ct; i++)
-		delete lolas[i];
-
-
-	cout << "Bye.\n";
+	Queue<Worker*> lolas[SIZE]; //created a queue of pointers to the Worker
+	
+	
 	return 0;
 }
-
