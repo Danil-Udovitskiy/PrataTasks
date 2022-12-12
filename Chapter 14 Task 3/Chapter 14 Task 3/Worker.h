a6b0ff12cd5bae582a4cd1244dc5f57593cac1d5
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-12 19:34:14 +0200

    Added Singer class declaration and definition Tested by creating a Singer object from the ABS Worker (using the program from listing 14.12)

--- Chapter 14 Task 3/Chapter 14 Task 3/Worker.h
+++ Chapter 14 Task 3/Chapter 14 Task 3/Worker.h
@@ -81,3 +81,29 @@ public:
 	void Set();
 	void Show() const;
 };
+
+
+//+++
+class Singer : virtual public Worker
+{
+protected:
+
+	enum { other, alto, contralto, soprano, bass, baritone, tenor };
+	enum { Vtypes = 7 };
+	void Data() const;
+	void Get();
+
+private:
+
+	static const char* pv[Vtypes]; // строковые эквиваленты видов голосов 
+	int voice;
+
+public:
+
+	Singer() : Worker(), voice(other) {}
+	Singer(const std:: string & s, long n, int v = other): Worker(s, n), voice(v) {}
+	Singer(const Worker& wk, int v = other): Worker(wk), voice(v) { }
+
+	void Set();
+	void Show() const;
+};
