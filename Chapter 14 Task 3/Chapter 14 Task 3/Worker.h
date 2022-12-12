2d8bd2aa5398277aa5627dbdc2dec5c8dbf8fefa
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-12 18:22:22 +0200

    Added declaration and definition of class Waiter Tested by creating a Waiter object from the ABS Worker (using the program from listing 14.12)

--- Chapter 14 Task 3/Chapter 14 Task 3/Worker.h
+++ Chapter 14 Task 3/Chapter 14 Task 3/Worker.h
@@ -52,9 +52,32 @@ protected:
 public:
 
 	Worker() : fullname("no one"), id(0L) {}
-	Worker(const std::string& s, long n)
-		: fullname(s), id(n) {}
+	Worker(const std::string& s, long n): fullname(s), id(n) {}
+
 	virtual ~Worker() = 0; // чистая виртуальная функция
 	virtual void Set() = 0;
 	virtual void Show() const = 0;
+};
+
+
+//++
+class Waiter : virtual public Worker
+{
+private:
+
+	int panache;
+
+protected:
+
+	void Data() const;
+	void Get();
+
+public:
+
+	Waiter() : Worker(), panache(0) {}
+	Waiter (const std::string& s, long n, int p = 0): Worker(s, n), panache(p) {}
+	Waiter(const Worker& wk, int p = 0): Worker(wk), panache(p) {}
+	
+	void Set();
+	void Show() const;
 };
