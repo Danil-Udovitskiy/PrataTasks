b2e4870471b8e7260359f26a063e598f6cfe3994
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-22 14:47:44 +0200

    Wrote the declaration and definition of the base class, the hmean exception class is inherited from it Hmean is also modified taking into account inheritance from the base class + 1 catch with an object of the base type has been introduced in the main

---
+++ Chapter 15 Task 3/Chapter 15 Task 3/Base_Class.h
@@ -0,0 +1,51 @@
+#pragma once
+
+//Listing 15.10
+//Base class and exeption class bad_hmean
+#include <iostream>
+#include <stdexcept>
+
+
+//base class (child of logic_error)
+class Base :public std::logic_error
+{
+private:
+	//2 argument values
+	double v1;
+	double v2;
+
+public:
+	//base class constructor
+	Base(double a = 0.0, double b = 0.0, const char* what = nullptr) :v1(a), v2(b), logic_error(what) {};
+
+	// the method is virtual, since it will be overridden in the descendant classes
+	virtual const char* what();
+
+};
+
+inline const char* Base::what()
+{
+	std::cout << "Base Class(" << v1 << ", " << v2 << "): " << "invalid arguments: a = -b\n";
+	return logic_error::what();
+}
+
+
+
+
+class bad_hmean :public Base //class of exception derived from base
+{
+
+public:
+
+	//initialize bad_hmean class values with base class values
+	bad_hmean(double a = 0, double b = 0, const char* what = nullptr) : Base(a, b, what) { };
+
+	// display method (overridden)
+	const char* what();
+};
+
+inline const char* bad_hmean::what()
+{
+	std::cout << "hmean() invalid arguments: a = -b\n";
+	return Base::what(); //output data from the what() method of the base class
+}
---
+++ Chapter 15 Task 3/Chapter 15 Task 3/Chapter 15 Task 3.cpp
@@ -0,0 +1,56 @@
+﻿// Chapter 15 Task 3.cpp 
+
+#include <iostream> 
+#include <cmath>
+#include  "Base_Class.h"
+
+
+double hmean(double a, double b);
+
+int main()
+{
+	using std::cout;
+	using std::cin;
+	using std::endl;
+	double x, y, z;
+	cout << "Enter two numbers: ";
+	while (cin >> x >> y) // enter 2 numbers
+	{
+		try {						// begin of block - try 
+
+			z = hmean(x, y);
+			cout << "Harmonic mean of " << x << " and " << y
+				<< " is " << z << endl; // harmonic mean
+
+			cout << "Enter next set of numbers <q to quit>: "; // enter next 2 numbers
+
+		}							// end of block - try 
+
+		
+		// +++ 
+		// use base class type to catch hmean exception
+		catch (Base& base_obj)		// begin of block - catch 
+		{
+			cout << base_obj.what();
+			cout << "\nEnd.\n";
+			break;
+		}
+
+	}
+
+	cout << "Bye!\n";
+	return 0;
+}
+
+
+double hmean(double a, double b)
+{
+	if (a == -b)
+	{
+		throw bad_hmean(a, b, "bad arguments to hmean()");
+		//you can also pass 2 arguments, so logic_error::what initialized to nullptr - throw bad_hmean(a, b), but in this case exeption will not be thrown
+	}
+
+	return 2.0 * a * b / (a + b);
+}
+
