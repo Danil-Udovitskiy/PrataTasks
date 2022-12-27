3b968a67e5c6c51368fc728cde9192ea8a015514
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-27 21:39:06 +0200

    Added a loop that goes through each element of the string variable (by using index) and converts uppercase letters to lowercase

---
+++ Chapter 16 Task 2/Chapter 16 Task 2/Chapter 16 Task 2.cpp
@@ -0,0 +1,43 @@
+﻿// Chapter 16 Task 2.cpp 
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+bool palindrom(std::string& w)
+{
+    int size = w.size();
+    for (int i = 0; i < size; i++)
+    {
+       if (w[i] == toupper(w[i]))
+        {
+            w[i] = tolower(w[i]);
+        }
+   }
+
+
+    std::string temp = w;
+    std::reverse(w.begin(), w.end());
+
+    if (temp == w)
+    {
+        std::cout << "palindrome\n";
+        return true;
+    }
+    else
+    {
+        std::cout << "no palindrome\n";
+        return false;
+    }
+}
+
+
+int main()
+{
+    std::string word;
+    std::cout << "Enter word : ";
+    std::cin >> word;
+
+    std::cout << palindrom(word) << "\n";
+
+}
