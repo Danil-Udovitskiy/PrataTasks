ee03c6a78e630a4c9de44ebd71fed49e9b8ba73e
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-28 22:37:19 +0200

    The program receives words from a file and writes them to a vector using push_back() After all the data from the file is written, I use the size() member function to determine the length of the word list

--- Chapter 16 Task 3/Chapter 16 Task 3/Chapter 16 Task 3.cpp
+++ Chapter 16 Task 3/Chapter 16 Task 3/Chapter 16 Task 3.cpp
@@ -10,90 +10,115 @@
 
 //++
 #include <vector>
+#include <fstream>
 
-using std::string;
-
-//vector of strings
-const std::vector <string> wordlist = { "apiary", "beetle", "cereal",
-"danger", "ensign", "florid", "garage", "health", "insult",
-"jackal", "keeper", "loaner", "manage", "nonce", "onset",
-"plaid", "quilt", "remote", "stolid", "train", "useful",
-"valid", "whence", "xenon", "yearn", "zippy" };
-
-const int NUM = wordlist.size(); //get size of vector
 
 int main()
 {
-	using std::cout;
-	using std::cin;
-	using std::tolower;
-	using std::endl;
-	std::srand(std::time(0));
-	char play;
-	cout << "Will you play a word game? <y/n> "; // запуск игры в слова 
-	cin >> play;
-	play = tolower(play);
+	using std::string;
 
-	while (play == 'y')
+	std::ifstream file("d:\\File\\Hello.txt"); // open file for reading
+
+	//or we can create ifstream object and then open file for reading
+	//std::ifstream file;
+	//file.open("d:\\File\\Hello.txt");
+
+	std::vector <string> wordlist; 
+
+	if (file.is_open()) // call the is_open() method
 	{
-		string target = wordlist[std::rand() % NUM];
-		int length = target.length();
-		string attempt(length, '-');
-		string badchars;
-		int guesses = 6;
-		cout << "Guess my secret word. It has " << length
-			<< " letters, and you guess\n"
-			<< "one letter at a time. You get " << guesses
-			<< " wrong guesses.\n";
-		cout << "Your word: " << attempt << endl; // вывод слова 
+		std::cout << "FILE OPEN!\n\n" << std::endl;
 
-		while (guesses > 0 && attempt != target)
+		string next; // object to which the words are written in turn
+
+		while (file >> next) //use the >> operator (same as when reading from the console)
 		{
-			char letter;
-			cout << "Guess a letter: ";
-			cin >> letter;
-			if (badchars.find(letter) != string::npos
-				|| attempt.find(letter) != string::npos)
-			{
-				cout << "You already guessed that. Try again.\n";
-				continue;
-			}
-			int loc = target.find(letter);
-			if (loc == string::npos)
-			{
-				cout << "Oh, bad guess !\n";
-				--guesses;
-				badchars += letter; // добавить к строке 
-			}
-			else
-			{
-				cout << "Good guess!\n";
-				attempt[loc] = letter;
-				// Проверить, не появляется ли буква еще раз 
-				loc = target.find(letter, loc + 1);
-				while (loc != string::npos)
-				{
-					attempt[loc] = letter;
-					loc = target.find(letter, loc + 1);
-				}
-			}
-			cout << "Your word: " << attempt << endl;
-			if (attempt != target)
-			{
-				if (badchars.length() > 0)
-					cout << "Bad choices: " << badchars << endl;
-				cout << guesses << " bad guesses left\n";
-			}
+			wordlist.push_back(next); //fill vector with data (words) from file
 		}
-		if (guesses > 0)
-			cout << "That's right!\n";
-		else
-			cout << "Sorry, the word is " << target << " . \n";
-		cout << "Will you play another? <y/n> ";
+
+		const int NUM = wordlist.size(); //get size of vector after adding all data(words) from file to it
+
+
+
+		using std::cout;
+		using std::cin;
+		using std::tolower;
+		using std::endl;
+		std::srand(std::time(0));
+		char play;
+		std::cout << "Will you play a word game? <y/n> "; // start the word game
 		cin >> play;
 		play = tolower(play);
+
+		while (play == 'y')
+		{
+			string target = wordlist[std::rand() % NUM];
+			int length = target.length();
+			string attempt(length, '-');
+			string badchars;
+			int guesses = 6;
+			std::cout << "Guess my secret word. It has " << length
+				<< " letters, and you guess\n"
+				<< "one letter at a time. You get " << guesses
+				<< " wrong guesses.\n";
+			std::cout << "Your word: " << attempt << endl; // word output
+
+			while (guesses > 0 && attempt != target)
+			{
+				char letter;
+				std::cout << "Guess a letter: ";
+				cin >> letter;
+				if (badchars.find(letter) != string::npos
+					|| attempt.find(letter) != string::npos)
+				{
+					std::cout << "You already guessed that. Try again.\n";
+					continue;
+				}
+				int loc = target.find(letter);
+				if (loc == string::npos)
+				{
+					std::cout << "Oh, bad guess !\n";
+					--guesses;
+					badchars += letter; // add to string
+				}
+				else
+				{
+					std::cout << "Good guess!\n";
+					attempt[loc] = letter;
+					// Check if the letter appears again
+					loc = target.find(letter, loc + 1);
+					while (loc != string::npos)
+					{
+						attempt[loc] = letter;
+						loc = target.find(letter, loc + 1);
+					}
+				}
+				std::cout << "Your word: " << attempt << endl;
+				if (attempt != target)
+				{
+					if (badchars.length() > 0)
+						std::cout << "Bad choices: " << badchars << endl;
+					std::cout << guesses << " bad guesses left\n";
+				}
+			}
+			if (guesses > 0)
+				std::cout << "That's right!\n";
+			else
+				std::cout << "Sorry, the word is " << target << " . \n";
+			std::cout << "Will you play another? <y/n> ";
+			cin >> play;
+			play = tolower(play);
+		}
+		std::cout << "Bye\n";
+
 	}
-	cout << "Bye\n";
+
+	else
+	{
+		std::cout << "FILE NOT OPEN!\n\n" << std::endl;
+		return -1;
+	}
+
+	file.close(); // close the file
 	return 0;
 }
-
