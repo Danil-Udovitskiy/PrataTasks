c537f28f46cc08b26eea7a8b72ef49d9b0f85e15
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-30 02:29:24 +0200

    Wrote a function with an old-style interface - int reduce(long ar[], int n); With arguments: the name of the array and the number of elements in it. The function sorts an array and returns its size

---
+++ Chapter 16 Task 4/Chapter 16 Task 4/Chapter 16 Task 4.cpp
@@ -0,0 +1,41 @@
+﻿// Chapter 16 Task 4.cpp 
+#include <iostream>
+#include <algorithm> // std::sort()
+
+void show(int n)
+{ 
+    std::cout << n << " ";
+}
+
+
+int reduce(long ar[], int n)
+{
+
+
+    std::sort(ar, ar + n);
+
+    std::cout << "\nsize = ";
+    return n; // return array size
+
+}
+
+int main()
+{
+    const int size = 6;
+    long arr[size]{ 10,10,30,30,50,40 };
+
+    for (int i = 0; i < size; ++i)
+    {
+        show(arr[i]);
+    }
+
+
+
+    //After reduce (sort +  get size) and using for_each
+    std::cout << reduce(arr, size) << "\n";
+
+    std::for_each(std::begin(arr), std::end(arr), show);
+
+
+}
+
