ee47de799fe93123e24469eded0b5c96b459cafe
Danil Udovitskiy <udovickijdaniil@gmail.com>
2022-12-30 23:26:33 +0200

    Partial changes (not full version)

--- Chapter 16 Task 5/Chapter 16 Task 5/Chapter 16 Task 5.cpp
+++ Chapter 16 Task 5/Chapter 16 Task 5/Chapter 16 Task 5.cpp
@@ -20,9 +20,9 @@ int reduce(T ar[], int n)
 
     n = result - &ar[0];
 
-    std::for_each(&ar[0], result, show<T>); //perform explicit type inference of the show<T> template parameter
+    //std::for_each(&ar[0], result, show<T>); //perform explicit type inference of the show<T> template parameter
     
-    std::cout << "\nsize = ";
+    //std::cout << "\nsize = ";
     return n; // return array size
 }
 
@@ -34,31 +34,30 @@ int main()
     const int size = 6;
     long arr[size]{ 10,10,30,30,50,40 };
 
-    for (int i = 0; i < size; ++i)
-    {
-        show(arr[i]);
-    }
-
+    std::for_each(std::begin(arr), std::end(arr), show<long>);
 
 
     //After reduce (sort +  get size + unique) and using for_each
     std::cout << "\n\nreduce\n";
     std::cout << reduce(arr, size) << "\n";
 
+    std::cout << "size = " << reduce(arr, size) << "\n";
 
+    int size_new = reduce(arr, size);
+    
+    //std::for_each(std::begin(arr), &arr[0] + size_new, show);
 
 
     std::cout << "\nString array\n";
     const int size2 = 6;
     std::string arrS[size2]{ "aa","bb","bb","gg","cc","dd" };
 
-    for (int i = 0; i < size2; ++i)
-    {
-        show(arrS[i]);
-    }
+    std::for_each(std::begin(arrS), std::end(arrS), show<std::string>);
 
 
     //After reduce (sort +  get size + unique) and using for_each
     std::cout << "\n\nreduce\n";
     std::cout << reduce(arrS, size2) << "\n";
 }
+
+
