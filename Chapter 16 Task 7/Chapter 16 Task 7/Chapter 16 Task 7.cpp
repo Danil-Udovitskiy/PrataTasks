2b3754551f67f33baff8a16a6b06d6717319400d
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-01 19:10:08 +0200

    Wrote a Lotto() function that takes 2 arguments and returns a vector<int> object containing numbers sorted by order + Testing in main

---
+++ Chapter 16 Task 7/Chapter 16 Task 7/Chapter 16 Task 7.cpp
@@ -0,0 +1,27 @@
+﻿// Chapter 16 Task 7.cpp 
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+std::vector<int> Lotto(int values, int size)
+{
+
+    std::vector<int>a (size); // create a vector of 6 elements (0 by default)
+
+    sort(a.begin(), a.end()); // sort 
+
+    return a; // return object vector<int>
+}
+
+int main()
+{
+    std::vector<int> winners;
+    winners = Lotto(51, 6);
+
+    for (int i = 0; i < winners.size(); i++)
+    {
+        std::cout << "#" << i + 1 << " = " << winners.at(i) << "\n"; // use the at() function to refer to the cells
+    }
+}
