2115bc55e3372e84052f2d30b98e6803c90b2334
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-02 23:02:35 +0200

    According to the assignment about testing with large values, I removed the output of the contents of the containers and left only the runtime output (test with a value of 100,000)
    Task text
    (For representative results, use the largest array possible.
    For example, you can have 100,000, 1,000,000, and 10,000,000 items.)

--- Chapter 16 Task 9/Chapter 16 Task 9/Chapter 16 Task 9.cpp
+++ Chapter 16 Task 9/Chapter 16 Task 9/Chapter 16 Task 9.cpp
@@ -2,10 +2,8 @@
 
 #include <iostream>
 #include <vector>
-
 #include <cstdlib> // rand()
 #include <ctime> // time(), clock()
-
 #include <list>
 #include <algorithm> //sort()
 
@@ -18,19 +16,13 @@ int main()
 	std::vector <int> vi0;
 
 	//Fill vec random values
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 100000; i++)
     {
 		//int a = rand();
 		//vi0.push_back(a);
 		vi0.push_back(rand());
     }
 
-	//Show random values in vec
-	std::cout << "vi0\n";
-	for (int i = 0; i < vi0.size(); i++)
-	{
-		std::cout << vi0.at(i) << "\n";
-	}
 
 
 	//b. Create a second vi object of type vector<int> and an object li of type list<int>
@@ -39,24 +31,10 @@ int main()
 	//Created vi initialized with vector v (size + values)
     std::vector <int> vi(vi0);
 
-	//Show random values in vec (values of vi0)
-    std::cout << "\nvi\n";
-    for (int i = 0; i < vi.size(); i++)
-    {
-		
-        std::cout << vi.at(i)<< "\n";
-    }
-
 
 	//Created list li initialized with vector v (size + values)
     std::list <int> li(vi0.begin(), vi0.end());
 
-	//Show random values in list (values of vi0)
-	std::cout << "\nli\n";
-	for (auto i = li.begin(); i != li.end(); i++)
-	{
-		std::cout << *i << "\n";
-	}
 
 
 
@@ -68,12 +46,6 @@ int main()
 
 	std::sort(vi.begin(), vi.end());
 
-	std::cout << "\nvi + sort\n";
-	for (int i = 0; i < vi.size(); i++)
-	{
-
-		std::cout << vi.at(i) << "\n";
-	}
 
 	clock_t end = clock();
 
@@ -83,11 +55,6 @@ int main()
 	start = clock();
 	li.sort();
 	
-	std::cout << "\nli + sort\n";
-	for (auto i = li.begin(); i != li.end(); i++)
-	{
-		std::cout << *i << "\n";
-	}
 
 	end = clock();
 	std::cout << "time list sort() method - " << (double)(end - start) / CLOCKS_PER_SEC << "\n";
@@ -106,49 +73,19 @@ int main()
 	//Reinstalling li with the unsorted content of vi0
 	li.assign(vi0.begin(), vi0.end());
 
-	std::cout << "\nli\n";
-	for (auto i = li.begin(); i != li.end(); i++)
-	{
-		std::cout << *i << "\n";
-	}
-
 	//Measure the running time of a mixed operation of copying li to vi, sorting vi, and copying the result back to li.
 	start = clock();
 
 	//Copy li to vi
 	vi.assign(li.begin(), li.end());
-	std::cout << "\nCopy li to vi\n";
-
-	for (int i = 0; i < vi.size(); i++)
-	{
-
-		std::cout << vi.at(i) << "\n";
-	}
 
 	//Sort vi
 	std::sort(vi.begin(), vi.end());
-	std::cout << "\nSort vi 2\n";
-	for (int i = 0; i < vi.size(); i++)
-	{
-
-		std::cout << vi.at(i) << "\n";
-	}
 
 	//Copy the result back to li
 	li.assign(vi.begin(), vi.end());
 
 
-	std::cout << "\nCopy the result back to li\n";
-	for (auto i = li.begin(); i != li.end(); i++)
-	{
-		std::cout << *i << "\n";
-	}
-
-
-
 	end = clock();
 	std::cout << "time list copy+sort+copy back " << (double)(end - start) / CLOCKS_PER_SEC << "\n";
-
-
-
 }
