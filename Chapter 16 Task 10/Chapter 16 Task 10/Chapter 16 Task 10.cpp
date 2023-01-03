f8cd6a9707d836445304feb0e2e00e529bf738ff
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-03 03:00:37 +0200

    Completed the step C After the data entry step, I implemented a loop that provides the user with the following options for displaying books: in original order, in alphabetical order, in ascending order of rating, in ascending order of price, in descending order of price, and the ability to exit the program.

--- Chapter 16 Task 10/Chapter 16 Task 10/Chapter 16 Task 10.cpp
+++ Chapter 16 Task 10/Chapter 16 Task 10/Chapter 16 Task 10.cpp
@@ -52,40 +52,79 @@ int main()
 
 	if (books.size() > 0)
 	{
-		cout << "Thank you. You entered the following "
-			<< books.size() << " ratings:\n"
-			<< "Rating\tBook\tPrice\n";
+		cout << "\nThank you. You entered the "
+			<< books.size() << " packs of data\n";
 
-		for_each(books.begin(), books.end(), ShowReview);
-		sort(books.begin(), books.end());
 
-		cout << "Sorted by title:\nRating\tBook\tPrice\n";
-		// List of books sorted by title
-		for_each(books.begin(), books.end(), ShowReview);
-		sort(books.begin(), books.end(), worseThan);
+		bool indicator = true;
+		while (indicator == true) 
+		{
+			cout << "\nHow to Show ?\n"
+				<< "1 - in original order;\n"
+				<< "2 - in alphabetical order;\n"
+				<< "3 - in ascending order of rating;\n"
+				<< "4 - in ascending order of price;\n"
+				<< "5 - in descending order of price;\n"
+				<< "6 - exit.\n\n";
 
-		cout << "Sorted by rating:\nRating\tBook\tPrice\n";
-		// List of books sorted by rating
-		for_each(books.begin(), books.end(), ShowReview);
+			int choice;
+			cin >> choice;
+			while ((choice < 1) && (choice > 5))
+			{
+				cout << "Wrong input. Repeat: ";
+				cin >> choice;
+			}
 
-		//++
-		cout << "Sorted by price low-hi:\nRating\tBook\tPrice\n";
-		// List of books sorted by price low-hi
-		sort(books.begin(), books.end(), worseThan_price);
-		for_each(books.begin(), books.end(), ShowReview);
-		
-		//++
-		cout << "Sorted by price hi-low:\nRating\tBook\tPrice\n";
-		// List of books sorted by price hi-low
-		sort(books.begin(), books.end(), betterThan_price);
-		for_each(books.begin(), books.end(), ShowReview);
-		
-		random_shuffle(books.begin(), books.end());
-
-		cout << "After shuffling:\nRating\tBook\n";
-		// List of books after shuffling
-		for_each(books.begin(), books.end(), ShowReview);
+			switch (choice)
+			{
+			case 1:
+			{
+				cout << "\nRating\tBook\tPrice\n";
+				for_each(books.begin(), books.end(), ShowReview);
+				break;
+			}
+			case 2:
+			{
+				sort(books.begin(), books.end());
+				cout << "Sorted by title:\nRating\tBook\tPrice\n";
+				// List of books sorted by title
+				for_each(books.begin(), books.end(), ShowReview);
+				break;
+			}
+			case 3:
+			{
+				sort(books.begin(), books.end(), worseThan);
+				cout << "Sorted by rating:\nRating\tBook\tPrice\n";
+				// List of books sorted by rating
+				for_each(books.begin(), books.end(), ShowReview);
+				break;
+			}
+			case 4:
+			{
+				cout << "Sorted by price low-hi:\nRating\tBook\tPrice\n";
+				// List of books sorted by price low-hi
+				sort(books.begin(), books.end(), worseThan_price);
+				for_each(books.begin(), books.end(), ShowReview);
+				break;
+			}
+			case 5:
+			{
+				cout << "Sorted by price hi-low:\nRating\tBook\tPrice\n";
+				// List of books sorted by price hi-low
+				sort(books.begin(), books.end(), betterThan_price);
+				for_each(books.begin(), books.end(), ShowReview);
+				break;
+			}
+			default:
+			{
+				indicator = false;
+				break;
+			}
+			}
+		}
 	}
+
+
 	else
 	{
 		cout << "No entries. ";
