6a660c3d79f64988df06ff93f427ea0efde6b36f
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-06 00:19:56 +0200

    The part that counts the number of characters up to the first $ character in the string

---
+++ Chapter 17 Task 1/Chapter 17 Task 1/Chapter 17 Task 1.cpp
@@ -0,0 +1,21 @@
+﻿// Chapter 17 Task 1.cpp 
+
+#include <iostream>
+
+int main()
+{
+
+	char symbol;
+	int counter = 0;
+
+
+	std::cout << "Enter symbol : ";
+	while (std::cin >> symbol && symbol != '$')
+	{
+		counter++;
+		std::cout << "Enter next symbol : ";
+	}
+
+	std::cout << "Total symbols = " << counter << "\n";
+
+}
