c570e865b3e713c628274fc235cfbbd8255679e4
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-06 02:06:31 +0200

    Read all symbols (read enter like symbol too)

--- Chapter 17 Task 1/Chapter 17 Task 1/Chapter 17 Task 1.cpp
+++ Chapter 17 Task 1/Chapter 17 Task 1/Chapter 17 Task 1.cpp
@@ -7,22 +7,14 @@ int main()
 	char symbol;
 	int counter = 0;
 
-	std::cout << "Enter symbols : ";
+	std::cout << "Enter symbols :\n";
 
 	//using the peek() function, we return the next input character without extracting it from the input stream
 	while (symbol = std::cin.peek() != '$') // "look" ahead
 	{
+		//read all symbols (read enter like symbol too)
 		std::cin.get(symbol);
-		
-		//do not read one enter (only after any symbol)
-		if (symbol != '\n')
-		{
-			counter++;
-		}
-		else
-		{
-			std::cout << "Enter next symbols : ";
-		}
+		counter++;
 	}
 
 	std::cout << "Total symbols before \"$\" = " << counter << "\n";
