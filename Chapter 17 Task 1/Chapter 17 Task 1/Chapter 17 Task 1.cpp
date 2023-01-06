c4e534af2f538854f04516f7b011b7bbd383e470
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-06 13:09:31 +0200

    Without using char type variable

--- Chapter 17 Task 1/Chapter 17 Task 1/Chapter 17 Task 1.cpp
+++ Chapter 17 Task 1/Chapter 17 Task 1/Chapter 17 Task 1.cpp
@@ -4,16 +4,15 @@
 
 int main()
 {
-	char symbol;
 	int counter = 0;
 
 	std::cout << "Enter symbols :\n";
 
 	//using the peek() function, we return the next input character without extracting it from the input stream
-	while (symbol = std::cin.peek() != '$') // "look" ahead
+	while (std::cin.peek() != '$') // "look" ahead
 	{
 		//read all symbols (read enter like symbol too)
-		std::cin.get(symbol);
+		std::cin.get();
 		counter++;
 	}
 
