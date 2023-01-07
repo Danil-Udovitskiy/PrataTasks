718aaa1c0d6efc7ecabd17f10646b0e0c5b3c91d
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-07 03:20:51 +0200

    If the name of the file to write data to is not passed on the command line (the name of the current file is substituted automatically) Because argc always defaults to 1

---
+++ Chapter 17 Task 2/Chapter 17 Task 2/Chapter 17 Task 2.cpp
@@ -0,0 +1,13 @@
+﻿// Chapter 17 Task 2.cpp 
+
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1)  //if the file name is not passed on the command line (substituted automatically)
+	{
+		std::cerr << "Name of file    " << *argv << "\n";
+		return 1;
+	}
+}
