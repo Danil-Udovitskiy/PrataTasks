2cff490fc0d381521f426c90844703943c6fc109
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-10 00:29:52 +0200

    We open 2 files for reading, which are filled with data from the arrays mat_names and pat_names + checks for successful opening

--- Chapter_17_Task_5/Chapter_17_Task_5/Chapter_17_Task_5.cpp
+++ Chapter_17_Task_5/Chapter_17_Task_5/Chapter_17_Task_5.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <cstring>
 
+
 int main()
 {
     //three text files for input 
@@ -33,11 +34,36 @@ int main()
 
         //fill files with array data using the write method
         for (int i = 0; i < 3; i++)
-            fout1.write((char*)mat_names[i], sizeof mat_names[i]);
+            fout1.write((char*)mat_names[i], sizeof (mat_names[i]));
         for (int i = 0; i < 3; i++)
-            fout2.write((char*)pat_names[i], sizeof pat_names[i]);
+            fout2.write((char*)pat_names[i], sizeof (pat_names[i]));
     }
 
 	fout1.close();
 	fout2.close();
+
+
+
+    std::ifstream fin1; //create an ifstream object
+    fin1.open(first, std::ios_base::in); //open with file mode iosbase::in (open file for reading)
+
+    std::ifstream fin2; //create an ifstream object
+    fin2.open(second, std::ios_base::in); //open with file mode iosbase::in (open file for reading)
+
+    //check if the two files were opened successfully
+    if (!fin1.is_open())
+    {
+        std::cerr << "FILE NOT OPEN   " << first << "\nSTOP\n";
+        return 1;
+    }
+
+    if (!fin1.is_open())
+    {
+        std::cerr << "FILE NOT OPEN   " << second << "\nSTOP\n";
+        return 1;
+    }
+
+
+   
+
 }
