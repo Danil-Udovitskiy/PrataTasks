7b0ecd441f81437f6b247b008f3ea246989b7ee8
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-10 23:26:14 +0200

    Using (Chapter 14 task 5) for the current task

---
+++ Chapter_17_Task_6/Chapter_17_Task_6/Chapter_17_Task_6.cpp
@@ -0,0 +1,48 @@
+﻿// Chapter_17_Task_6.cpp 
+
+
+#include <iostream>
+#include "Emp.h"
+
+int main()
+{
+	std::cout << "Employee\n\n";
+	employee em("Trip", "Harris", "Thumper");
+	std::cout << em << "\n";
+	em.ShowAll();
+
+
+	std::cout << "\n\nManager\n\n";
+	manager ma("Amorphia", "Spindragon", "Nuancer", 5);
+	std::cout << ma << "\n";
+	ma.ShowAll();
+
+
+	std::cout << "\n\nFink\n\n";
+	fink fi("Matt", "Oggs", "Oiler", "JunoBarr");
+	std::cout << fi << "\n";
+	fi.ShowAll();
+
+
+	std::cout << "\n\nHighfink\n\n";
+	highfink hf(ma, "Curly Kew");
+	hf.ShowAll();
+
+
+	std::cout << "\n\nPress a key for next phase:\n";
+
+	std::cin.get();
+	highfink hf2;
+	hf2.SetAll();
+	std::cout << "Using an abstr_emp * pointer:\n";
+	// Using a pointer abstr_emp * 
+	Abstr_emp* tri[4] = { &em, &fi, &hf, &hf2 };
+	for (int i = 0; i < 4; i++)
+		tri[i]->ShowAll();
+
+
+
+
+	return 0;
+
+}
---
+++ Chapter_17_Task_6/Chapter_17_Task_6/Emp.h
@@ -0,0 +1,91 @@
+#pragma once
+
+//header file for abstr_emp class and its child classes
+#include <iostream> 
+#include <string> 
+
+class Abstr_emp
+{
+private:
+	std::string fname; // name abstr_emp 
+	std::string lname; // last name abstr_emp 
+	std::string job;
+
+public:
+
+	Abstr_emp();
+	Abstr_emp(const std::string& fn, const std::string& ln, const std::string& j);
+
+	virtual void ShowAll() const;	// outputs all data with names
+	virtual void SetAll();			// requests values from the user
+
+	friend std::ostream& operator<<(std::ostream& os, const Abstr_emp& e);  // Displays only first and last name
+
+	virtual ~Abstr_emp() = 0; // virtual base class
+};
+
+
+class employee : public Abstr_emp
+{
+public:
+	employee();
+	employee(const std::string& fn, const std::string& ln, const std::string& j);
+	virtual void ShowAll() const;
+	virtual void SetAll();
+};
+
+
+
+class manager : virtual public Abstr_emp
+{
+private:
+	int inchargeof; // number of managed abstr_emp
+
+protected:
+	int InChargeOf() const { return inchargeof; } // cout
+	int& InChargeOf() { return inchargeof; } // cin
+
+public:
+	manager();
+	manager(const std::string& fn, const std::string& ln, const std::string& j, int ico = 0);
+	manager(const Abstr_emp& e, int ico);
+	manager(const manager& m);
+	virtual void ShowAll() const;
+	virtual void SetAll();
+};
+
+
+
+class fink : virtual public Abstr_emp
+{
+private:
+	std::string reportsto; // report to
+
+protected:
+	const std::string ReportsTo() const { return reportsto; }
+	std::string& ReportsTo() { return reportsto; }
+public:
+	fink();
+	fink(const std::string& fn, const std::string& ln, const std::string& j, const std::string& rpo);
+	fink(const Abstr_emp& e, const std::string& rpo);
+	fink(const fink& e);
+
+	virtual void ShowAll() const;
+	virtual void SetAll();
+};
+
+
+
+class highfink : public manager, public fink // supervision of managers
+{
+public:
+	highfink();
+	highfink(const std::string& fn, const std::string& ln, const std::string& j, const std::string& rpo, int ico);
+	highfink(const Abstr_emp& e, const std::string& rpo, int ico);
+	highfink(const fink& f, int ico);
+	highfink(const manager& m, const std::string& rpo);
+	highfink(const highfink& h);
+
+	virtual void ShowAll() const;
+	virtual void SetAll();
+};
