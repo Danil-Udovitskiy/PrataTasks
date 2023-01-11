4563a19824e886fa7152f999874452ae397c91ee
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-11 03:36:29 +0200

    Declared and defined a functor Store class with an overridden operation () to write to the data file

--- Chapter_17_Task_7/Chapter_17_Task_7/Chapter_17_Task_7.cpp
+++ Chapter_17_Task_7/Chapter_17_Task_7/Chapter_17_Task_7.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
 
 
 void ShowStr(const std::string& str)
@@ -11,7 +12,25 @@ void ShowStr(const std::string& str)
 }
 
 
+class Store //functor class defines operator()()
+{
+private:
+    //const or reference data member cause implicit copy assignment operator to be deleted
+    std::ofstream& fill_data;
 
+public:
+    Store(std::ofstream& fout) : fill_data(fout) {}; //indicates an ifstream object
+    void operator()(const std::string& str); //indicates the string to be written
+};
+
+void Store::operator()(const std::string& str)
+{
+    //write down the size of the string first, and then its content
+    int len = str.size(); //size
+
+    fill_data.write((char*)&len, sizeof(std::size_t)); // save length (size)
+    fill_data.write(str.data(), len) << "\n"; // save characters (content)
+}
 
 int main()
 {
@@ -29,13 +48,13 @@ int main()
 
 
 
-    /*
-    // Сохранить в файле
-    ofstream fout("strings.dat", ios_base::out | ios_base::binary);
-    for_each(vostr.begin(), vostr.end(), Store(fout));
+    
+    // Save to file
+    ofstream fout("strings.dat.txt", ios_base::out | ios_base::binary);
+    for_each(vostr.begin(), vostr.end(), Store (fout));
     fout.close();
 
-
+    /*
     // Восстановить содержимое файла
     vector<string> vistr;
     ifstream fin("strings.dat", ios_base::in | ios_base:: binary);
