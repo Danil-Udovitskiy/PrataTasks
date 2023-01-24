389cc01fb8d4d3e431216e285b3f68ef976d3ca4
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-01-24 02:53:19 +0200

    Putting all force values into a vector Sorting for correct comparison of pairs of numbers We pass along the vector and compare the results of the differences of pairs of numbers

---
+++ Horse-Racing_Duals(CodinGame)/Horse-Racing_Duals(CodinGame)/Horse-Racing_Duals(CodinGame).cpp
@@ -0,0 +1,44 @@
+﻿// Horse-Racing_Duals(CodinGame).cpp 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    std::vector<int> powers;
+
+    int n; 
+    cin >> n; cin.ignore();
+    for (int i = 0; i < n; i++) 
+    {
+        int pi;
+        cin >> pi; cin.ignore();
+        powers.push_back(pi);
+    }
+  
+    sort(powers.begin(), powers.end()); // sort the vector to correctly compare values in the future
+    // because min_difference can be set to a larger value than will be farther among pairs of numbers
+
+
+    int min_difference = abs(powers[0] - powers[1]); // difference between first two elements (serves as minimum value)
+    int current_difference = 0;
+
+
+    for (int i = 1; i < powers.size() - 1; i++) // the loop is limited by the penultimate value of the vector, since we will refer to its last element in the last iteration
+    // in the case of i < powers.size() , when accessing powers[i + 1], the vector will be out of bounds
+    
+    {
+        current_difference = abs(powers[i] - powers[i + 1]); //difference between 2 and 3 vector elements (3/4, 4/5 ...)
+        
+        if (current_difference < min_difference) // if the difference between the first two elements is greater than the current one
+        {
+            min_difference = current_difference; // update the minimum difference value
+        }
+    }
+
+    cout << min_difference << endl;
+}
