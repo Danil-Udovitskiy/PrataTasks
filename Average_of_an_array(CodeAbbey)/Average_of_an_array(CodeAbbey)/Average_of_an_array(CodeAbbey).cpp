9516a4e8edb7a412801c858d2d5c2b86c26b2f15
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-03 03:16:36 +0200

    CodeAbbey (Avarage of array not ready)

---
+++ Average_of_an_array(CodeAbbey)/Average_of_an_array(CodeAbbey)/Average_of_an_array(CodeAbbey).cpp
@@ -0,0 +1,38 @@
+﻿// Average_of_an_array(CodeAbbey).cpp 
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	
+	//int size = 0;
+	double size = 0.0;
+	std::cout << "Enter number of values : ";
+	std::cin >> size;
+
+	std::vector<int> vec(size);
+	//vec.resize(size);
+
+
+	int number = 0;
+	std::cout << "Enter numbers : ";
+	for (int i = 0; i < vec.size(); i++)
+	{
+		std::cin >> number;
+		vec[i] = number;
+	}
+
+
+	//int sum = 0;
+	double sum = 0.0;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		sum = sum + vec[i];
+	}
+
+
+	double avarage = sum / size;
+	std::cout << "avarage = " << round(avarage) << "\n";
+}
+
---
+++ Maximum_of_array(CodeAbbey)/Maximum_of_array(CodeAbbey)/Maximum_of_array(CodeAbbey).cpp
@@ -0,0 +1,37 @@
+﻿// Maximum_of_array(CodeAbbey).cpp 
+
+#include <iostream>
+
+int main()
+{
+    const int size = 300;
+    int array[size];
+
+    int number;
+    std::cout << "Enter values: ";
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> number;
+        array[i] = number;
+    }
+
+    int min = array[0];
+    int max = array[0];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+
+    //std::cout << "maximal = " << max << " " << "minimal = " << min << "\n";
+    std::cout << max << " " << min << "\n";
+}
