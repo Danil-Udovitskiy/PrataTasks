410d9f15ddcafd1371c0435b5622f85b7adf24b5
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-04 02:27:25 +0200

    Add convert all letters are lowercase except for first character and except character after dot + space(a. A)

--- Text_formatting(CodinGame)/Text_formatting(CodinGame)/Text_formatting(CodinGame).cpp
+++ Text_formatting(CodinGame)/Text_formatting(CodinGame)/Text_formatting(CodinGame).cpp
@@ -14,13 +14,15 @@ int main()
 	cerr << intext << "\n"; //input data display
 
 	// Capitalize the first letter
-	intext[0] = toupper(intext[0]);
+	//intext[0] = toupper(intext[0]);
 
+
+	bool isLower = true;
 
 	for (int i = 0; i < intext.size(); i++)
 	{
 		//add space after comma only if missing
-		if (intext[i] == ',' && intext[i + 1] != ' ')
+		if (intext[i] == ',' && intext[i + 1] != ' ' && intext[i + 2] != ' ') //++&& intext[i + 2] != ' '
 		{
 			intext.insert(i + 1, std::string(" "));
 		}
@@ -40,13 +42,14 @@ int main()
 			intext.erase(i + 1, 1);
 		}
 
-
 		//capital letter after dot and space
 		else if (intext[i] == '.' && i != intext.size() - 1 && intext[i + 1] == ' ')
 		{
 
 			intext[i + 2] = toupper(intext[i + 2]);
 			intext.erase(i - 1, 1);
+			isLower = true;
+			//break;
 		}
 
 		//remove space before comma
@@ -55,16 +58,56 @@ int main()
 			intext.erase(i - 1, 1);
 
 		}
+
 		//remove space before dot
 		else if (intext[i] == '.' && intext[i - 1] == ' ')
 		{
 			intext.erase(i - 1, 1);
 		}
 
+		//all letters are lowercase except for 1 character and except character after dot + space(a. A)
+		if (isLower == true && isalpha(intext[i]))
+		{
+			intext[i] = toupper(intext[i]); 
+			isLower = false;
+		}
+
+		//to lower case
+		else
+		{
+			intext[i] = tolower(intext[i]);
+		}
+
+		//to uppercase after a dot with a space
+		if (intext[i] == '.' && intext[i + 1] == ' ')
+		{
+			intext[i + 2] = toupper(intext[i + 2]);
+			isLower = true;
+		}
+		
+
+
+		//Doesnt work
+		//++++
+		//remove duplicate coma
+		//if (intext[i] == ',' && intext[i + 1] == ',') // || intext[i] == ',' && intext[i + 2] == ','
+		//{
+		//	intext.erase(i + 1, 1);
+		//}
+		////remove duplicate dot
+		//else if (intext[i] == '.' && intext[i + 1] == '.') // || intext[i] == '.' && intext[i + 2] == '.'
+		//{
+		//	intext.erase(i + 1, 1);
+		//}
+		////remove duplicate space
+		//else if (intext[i] == ' ' && intext[i + 1] == ' ')
+		//{
+		//	intext.erase(i + 1, 1);
+		//}
 
 	}
+
 
 	cout << intext << endl; //formatted text output
 
 }
-
