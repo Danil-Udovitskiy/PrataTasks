8c7f57b3399a55be1677661636ba2cd70d680bc0
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-07 00:27:15 +0200

    +Pythagorean Theorem

---
+++ Pythagorean_theorem(CodeAbbey)/Pythagorean_theorem(CodeAbbey)/Pythagorean_theorem(CodeAbbey).cpp
@@ -0,0 +1,44 @@
+﻿// Pythagorean_theorem(CodeAbbey).cpp 
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int size = 0;
+	std::cout << "Enter number of tests: ";
+	std::cin >> size;
+
+	std::vector<char>angles;
+
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+
+	for (int i = 0; i < size; i++)
+	{
+
+		std::cout << "enter 3 values : ";
+		std::cin >> a >> b >> c;
+
+		if ((c * c) < ((a * a) + (b * b)))
+		{
+			angles.push_back('A');
+		}
+		else if ((c * c) > ((a * a) + (b * b)))
+		{
+			angles.push_back('O');
+		}
+		else if ((c * c) == ((a * a) + (b * b)))
+		{
+			angles.push_back('R');
+		}
+	}
+
+
+	for (int i = 0; i < angles.size(); i++)
+	{
+		std::cout << angles[i] << " ";
+	}
+}
