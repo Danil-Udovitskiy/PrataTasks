9a2417c67a244fc88cd94f9c7a84294b7e630579
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-10 00:51:12 +0200

    +Array_checksum

---
+++ Array_checksum(CodeAbbey)/Array_checksum(CodeAbbey)/Array_checksum(CodeAbbey).cpp
@@ -0,0 +1,32 @@
+﻿// Array_checksum(CodeAbbey).cpp 
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int size = 0;
+    std::cout << "Enter size: ";
+    std::cin >> size;
+
+    int number = 0;
+
+    std::vector<int>numbers;
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> number;
+        number %= 10000007;         // prevent overflow
+        numbers.push_back(number);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        sum += numbers[i];
+        sum *= 113;
+        sum %= 10000007;
+    }
+
+    std::cout << "sum = " << sum << "\n";
+}
