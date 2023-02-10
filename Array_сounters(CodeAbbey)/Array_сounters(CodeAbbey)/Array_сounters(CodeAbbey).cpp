7910d8c4ffb2cdced57c98d5bca6561d1755c734
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-11 01:58:19 +0200

    Changed count of duplicate values

--- Array_сounters(CodeAbbey)/Array_сounters(CodeAbbey)/Array_сounters(CodeAbbey).cpp
+++ Array_сounters(CodeAbbey)/Array_сounters(CodeAbbey)/Array_сounters(CodeAbbey).cpp
@@ -30,86 +30,11 @@ int main()
 
     for (int i = 0; i < size; i++)
     {
-        if (numbers[i] == 1)
-        {
-            counters[0]++;
-        }
-        else if (numbers[i] == 2)
-        {
-            counters[1]++;
-        }
-        else if (numbers[i] == 3)
-        {
-            counters[2]++;
-        }
-        else if (numbers[i] == 4)
-        {
-            counters[3]++;
-        }
-        else if (numbers[i] == 5)
-        {
-            counters[4]++;
-        }
-        else if (numbers[i] == 6)
-        {
-            counters[5]++;
-        }
-        else if (numbers[i] == 7)
-        {
-            counters[6]++;
-        }
-        else if (numbers[i] == 8)
-        {
-            counters[7]++;
-        }
-        else if (numbers[i] == 9)
-        {
-            counters[8]++;
-        }
-        else if (numbers[i] == 10)
-        {
-            counters[9]++;
-        }
-        else if (numbers[i] == 11)
-        {
-            counters[10]++;
-        }
-        else if (numbers[i] == 12)
-        {
-            counters[11]++;
-        }
-        else if (numbers[i] == 13)
-        {
-            counters[12]++;
-        }
-        else if (numbers[i] == 14)
-        {
-            counters[13]++;
-        }
-        else if (numbers[i] == 15)
-        {
-            counters[14]++;
-        }
-        else if (numbers[i] == 16)
-        {
-            counters[15]++;
-        }
-        else if (numbers[i] == 17)
-        {
-            counters[16]++;
-        }
-        else if (numbers[i] == 18)
-        {
-            counters[17]++;
-        }
-        else if (numbers[i] == 19)
-        {
-            counters[18]++;
-        }
-        else if (numbers[i] == 20)
-        {
-            counters[19]++;
-        }
+        //iterate over the vector (over all values)
+        int index = numbers[i] - 1; //refer to element 0, then subtract 1 from VALUE
+        //numbers[i] value itself no more than 20
+        //fill from element 0, so numbers[i]-1
+        counters[index]++;
     }
 
     for (int i = 0; i < counters.size(); i++)
