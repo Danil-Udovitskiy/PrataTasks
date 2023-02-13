d750f44acc75c0f3f1838ae0c66274854beb5e94
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-13 23:57:08 +0200

    +Collatz_sequence

---
+++ Collatz_sequence(CodeAbbey)/Collatz_sequence(CodeAbbey)/Collatz_sequence(CodeAbbey).cpp
@@ -0,0 +1,44 @@
+﻿// Collatz_sequence(CodeAbbey).cpp
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int size = 0;
+	std::cout << "Enter size : ";
+	std::cin >> size;
+
+	std::vector<int>results;
+
+	for (int i = 0; i < size; i++)
+	{
+		//std::cout << "Enter number : ";
+		int number = 0;
+		std::cin >> number;
+
+		int counter = 0;
+
+		while (number != 1)
+		{
+			if (number % 2 == 0)
+			{
+				int even = number / 2;
+				number = even;
+				counter++;
+			}
+			else
+			{
+				int odd = 3 * number + 1;
+				number = odd;
+				counter++;
+			}
+		}
+		results.push_back(counter);
+	}
+
+	for (int i = 0; i < results.size(); i++)
+	{
+		std::cout << results[i] << " ";
+	}
+}
