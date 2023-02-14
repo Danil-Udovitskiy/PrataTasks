df0f2709deff81b6fa5972648a0006422e12d6fe
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-14 21:54:28 +0200

    Solution of the problem in decimal notation (old solution kept)

--- Sum_of_digits(CodeAbbey)/Sum_of_digits(CodeAbbey)/Sum_of_digits(CodeAbbey).cpp
+++ Sum_of_digits(CodeAbbey)/Sum_of_digits(CodeAbbey)/Sum_of_digits(CodeAbbey).cpp
@@ -1,12 +1,53 @@
 ﻿// Sum_of_digits(CodeAbbey).cpp 
 
-//number to string conversion
 #include <iostream>
-#include <sstream> // stringstream
-#include <string>
 #include <vector>
 
 int main()
+{
+	int size = 0, a = 0, b = 0, c = 0;
+	std::cout << "Enter number of tests: ";
+	std::cin >> size;
+
+	std::vector<int> results;
+
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "Enter 3 values : ";
+		std::cin >> a >> b >> c;
+
+		int result = (a * b) + c;
+
+		int sum = 0;
+		int remainder = 0;
+
+		while (result != 0) 
+		{
+			remainder = result % 10; //remainder from dividing the number by 10
+			result = result / 10;
+			sum += remainder; //the remainder is the last digit in the number
+		}
+		
+		results.push_back(sum);
+	}
+
+	for (int i = 0; i < results.size(); i++)
+	{
+		std::cout << results[i] << " ";
+	}
+}
+
+
+
+
+
+//Old problem solution
+//number to string conversion
+
+//#include <sstream> // stringstream
+//#include <string>
+
+/*int main()
 {
 	int size = 0, a = 0, b = 0, c = 0;
 	std::cout << "Enter number of tests: ";
@@ -40,4 +81,4 @@ int main()
 	{
 		std::cout << results[i] << " ";
 	}
-}
+}*/
