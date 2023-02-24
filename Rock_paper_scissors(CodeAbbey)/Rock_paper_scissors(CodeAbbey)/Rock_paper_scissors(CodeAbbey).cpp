58557b7cd82473a165054a025c43517b738c30b3
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-02-24 02:54:29 +0200

    Additional version with processing options with a draw

--- Rock_paper_scissors(CodeAbbey)/Rock_paper_scissors(CodeAbbey)/Rock_paper_scissors(CodeAbbey).cpp
+++ Rock_paper_scissors(CodeAbbey)/Rock_paper_scissors(CodeAbbey)/Rock_paper_scissors(CodeAbbey).cpp
@@ -42,3 +42,65 @@ int main()
         std::cout << results[i] << " ";
     }
 }
+
+
+
+
+
+
+
+//With draws
+
+/*
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    int size = 0;
+    std::cout << "Enter size : ";
+    std::cin >> size;
+    std::cin.ignore();
+
+    std::vector <int>results;
+
+    for (int i = 0; i < size; i++)
+    {
+        std::string str;
+        //std::cout << "enter words :";
+        std::getline(std::cin, str);
+
+        int value2 = -2;
+
+        std::string temp = str.substr(value2 + str.size(), str.size());
+
+        int winner = 0;
+
+        if ((temp[0] == 'R' && temp[1] == 'S') || (temp[0] == 'S' && temp[1] == 'P') || (temp[0] == 'P' && temp[1] == 'R'))
+        {
+            winner = 1;
+        }
+        else if (((temp[0] == 'R' && temp[1] == 'R') || (temp[0] == 'S' && temp[1] == 'S') || (temp[0] == 'P' && temp[1] == 'P')))
+        {
+            winner = 0;
+        }
+        else
+        {
+            winner = 2;
+        }
+        results.push_back(winner);
+    }
+
+    for (int i = 0; i < results.size(); i++)
+    {
+        if (results[i] == 0)
+        {
+            std::cout << "DRAW ";
+        }
+        else
+        {
+            std::cout << results[i] << " ";
+        }
+    }
+}*/
