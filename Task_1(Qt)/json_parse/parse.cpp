ff971dbd94ed7008a851c6d67dd3e16b65c44c00
Danil Udovitskiy <udovickijdaniil@gmail.com>
2023-03-13 02:51:08 +0200

    parsing is not performed, falls on the first check
    // Check if there was an error while parsing
            if (jsonDoc.isNull())
            {
                QMessageBox::warning(nullptr, "Error", "Incorrect JSON string");
                return;
            }

--- Task_1(Qt)/json_parse/parse.cpp
+++ Task_1(Qt)/json_parse/parse.cpp
@@ -8,6 +8,145 @@
 #include <QStringList>
 #include "parse.h"
 
+ResultWindow::ResultWindow(QWidget *parent,QString str): QWidget(parent)
+{
+
+    firstName = new QLabel(tr("First users name:"));
+    firstName->setFixedWidth(100);
+
+    secondName = new QLabel(tr("Second users name:"));
+    secondName->setFixedWidth(100);
+
+    age = new QLabel(tr("Users age:"));
+    age->setFixedWidth(100);
+
+    fathersName = new QLabel(tr("Fathers name:"));
+    fathersName->setFixedWidth(100);
+
+    fathersAge = new QLabel(tr("Fathers age:"));
+    fathersAge->setFixedWidth(100);
+
+    fathersWeight = new QLabel(tr("Fathers weight:"));
+    fathersWeight->setFixedWidth(100);
+
+    mothersName = new QLabel(tr("Mothers name:"));
+    mothersName->setFixedWidth(100);
+
+    mothersAge = new QLabel(tr("Mothers age:"));
+    mothersAge->setFixedWidth(100);
+
+    mothersWeight = new QLabel(tr("Mothers weight:"));
+    mothersWeight->setFixedWidth(100);
+
+
+    QVBoxLayout* Layout1 = new QVBoxLayout;
+    Layout1->addWidget(firstName);
+    Layout1->addWidget(secondName);
+    Layout1->addWidget(age);
+    Layout1->addWidget(fathersName);
+    Layout1->addWidget(fathersAge);
+    Layout1->addWidget(fathersWeight);
+    Layout1->addWidget(mothersName);
+    Layout1->addWidget(mothersAge);
+    Layout1->addWidget(mothersWeight);
+
+
+
+    Parse(str);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(Layout1);
+    setLayout(mainLayout);
+    setWindowTitle("Parsed data");
+    resize(600, 400);
+}
+
+
+void ResultWindow::Parse(QString str)
+{
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(str.toUtf8());
+
+
+        // Check if there was an error while parsing
+        if (jsonDoc.isNull())
+        {
+            QMessageBox::warning(nullptr, "Error", "Incorrect JSON string");
+            return;
+        }
+
+        QJsonObject jsonObj = jsonDoc.object();
+        //check the correctness of the content for the keys
+
+        if (!jsonObj.contains("First name") || !jsonObj.contains("Second name") || !jsonObj.contains("Age") || !jsonObj.contains("Family"))
+        {
+            QMessageBox::warning(nullptr, "Error", "First name or Second name or Age or Family - JSON string does not contain all required keys");
+            return;
+        }
+
+        QJsonObject familyObj = jsonObj.value("Family").toObject();
+
+        if (!familyObj.contains("Father") || !familyObj.contains("Mother"))
+        {
+            QMessageBox::warning(nullptr, "Error", "Family - JSON string does not contain all required keys");
+            return;
+        }
+
+        QJsonObject fatherObj = familyObj.value("Father").toObject();
+
+        if (!fatherObj.contains("Name") || !fatherObj.contains("Age") || !fatherObj.contains("Weight"))
+        {
+            QMessageBox::warning(nullptr, "Error", "Father - JSON string does not contain all required keys");
+            return;
+        }
+
+        QJsonObject motherObj = familyObj.value("Mother").toObject();
+
+        if (!motherObj.contains("Name") || !motherObj.contains("Age") || !motherObj.contains("Weight"))
+        {
+            QMessageBox::warning(nullptr, "Error", "Mother - JSON string does not contain all required keys");
+            return;
+        }
+
+
+        //Get the values of the various keys from the jsonObj JSON object and store them in variables of type QString
+        QString firstName = jsonObj["First name"].toString();
+        QString secondName = jsonObj["Second name"].toString();
+        QString age = jsonObj["Age"].toString();
+
+        QJsonObject father = jsonObj["Family"].toObject()["Father"].toObject();
+        QString fathersName = father["Name"].toString();
+        QString fathersAge = QString::number(father["Age"].toInt());
+        QString fathersWeight = QString::number(father["Weight"].toDouble());
+
+        QJsonObject mother = jsonObj["Family"].toObject()["Mother"].toObject();
+        QString mothersName = mother["Name"].toString();
+        QString mothersAge = QString::number(mother["Age"].toInt());
+        QString mothersWeight = QString::number(mother["Weight"].toDouble());
+
+
+        firstNamea->setText(firstName);
+        secondNamea->setText(secondName);
+        agea->setText(age);
+        fathersNamea->setText(fathersName);
+        fathersAgea->setText(fathersAge);
+        fathersWeighta->setText(fathersWeight);
+        mothersNamea->setText(mothersName);
+        mothersAgea->setText(mothersAge);
+        mothersWeighta->setText(mothersWeight);
+
+        QStringList resultList;
+        resultList << firstName << firstNamea->text();
+        resultList << secondName << secondNamea->text();
+        resultList << age << agea->text();
+        resultList << fathersName << fathersNamea->text();
+        resultList << fathersAge << fathersAgea->text();
+        resultList << fathersWeight << fathersWeighta->text();
+        resultList << mothersName << mothersNamea->text();
+        resultList << mothersAge << mothersAgea->text();
+        resultList << mothersWeight << mothersWeighta->text();
+}
+
+
 MainWindow::MainWindow(QWidget* parent):QWidget(parent)
 {
     setWindowTitle(tr("JSON Parser"));
@@ -20,81 +159,15 @@ MainWindow::MainWindow(QWidget* parent):QWidget(parent)
     //button
     parseButton = new QPushButton(tr("Parse"));
     connect(parseButton, QPushButton::clicked, this, MainWindow::parseJson);
+
     layout->addWidget(parseButton);
 }
 
 
 void MainWindow::parseJson()
 {
-    QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonEdit->toPlainText().toUtf8());
-
-    // Check if there was an error while parsing
-    if (jsonDoc.isNull())
-    {
-        QMessageBox::warning(nullptr, "Error", "Incorrect JSON string");
-        return;
-    }
-
-    QJsonObject jsonObj = jsonDoc.object();
-    //check the correctness of the content for the keys
-
-    if (!jsonObj.contains("First name") || !jsonObj.contains("Second name") || !jsonObj.contains("Age") || !jsonObj.contains("Family"))
-    {
-        QMessageBox::warning(nullptr, "Error", "First name or Second name or Age or Family - JSON string does not contain all required keys");
-        return;
-    }
-
-    QJsonObject familyObj = jsonObj.value("Family").toObject();
-
-    if (!familyObj.contains("Father") || !familyObj.contains("Mother"))
-    {
-        QMessageBox::warning(nullptr, "Error", "Family - JSON string does not contain all required keys");
-        return;
-    }
-
-    QJsonObject fatherObj = familyObj.value("Father").toObject();
-
-    if (!fatherObj.contains("Name") || !fatherObj.contains("Age") || !fatherObj.contains("Weight"))
-    {
-        QMessageBox::warning(nullptr, "Error", "Father - JSON string does not contain all required keys");
-        return;
-    }
-
-    QJsonObject motherObj = familyObj.value("Mother").toObject();
-
-    if (!motherObj.contains("Name") || !motherObj.contains("Age") || !motherObj.contains("Weight"))
-    {
-        QMessageBox::warning(nullptr, "Error", "Mother - JSON string does not contain all required keys");
-        return;
-    }
-
-
-
-    //Get the values of the various keys from the jsonObj JSON object and store them in variables of type QString
-    QString firstName = jsonObj["First name"].toString();
-    QString secondName = jsonObj["Second name"].toString();
-    QString age = jsonObj["Age"].toString();
-
-    QJsonObject father = jsonObj["Family"].toObject()["Father"].toObject();
-    QString fathersName = father["Name"].toString();
-    QString fathersAge = QString::number(father["Age"].toInt());
-    QString fathersWeight = QString::number(father["Weight"].toDouble());
-
-    QJsonObject mother = jsonObj["Family"].toObject()["Mother"].toObject();
-    QString mothersName = mother["Name"].toString();
-    QString mothersAge = QString::number(mother["Age"].toInt());
-    QString mothersWeight = QString::number(mother["Weight"].toDouble());
-
-
-    //Create second window
-    QWidget* resultWindow = new QWidget;
-    QVBoxLayout* resultLayout = new QVBoxLayout(resultWindow);
-    QTextEdit* resultEdit = new QTextEdit;
-    resultEdit->setReadOnly(true);
-    resultLayout->addWidget(resultEdit);
-    resultWindow->setWindowTitle("Parsed data");
-    resultWindow->resize(600, 400);
-
+    ResultWindow* resultWindow = new ResultWindow;
+    //ResultWindow* resultWindow = new ResultWindow(this, jsonEdit->toPlainText());
 
     // Getting the geometry of the main window
     QRect mainRect = this->geometry();
@@ -106,19 +179,5 @@ void MainWindow::parseJson()
     // Set new coordinates for resultWindow window
     resultWindow->move(resultX, resultY);
 
-
-    QStringList resultList;
-    resultList << "First user's name: " + firstName + "\n";
-    resultList << "Second user's name: " + secondName + "\n";
-    resultList << "User's age: " + age + "\n";
-    resultList << "Father's name: " + fathersName;
-    resultList << "Father's age: " + fathersAge;
-    resultList << "Father's weight: " + fathersWeight + "\n";
-    resultList << "Mother's name: " + mothersName;
-    resultList << "Mother's age: " + mothersAge;
-    resultList << "Mother's weight: " + mothersWeight;
-
-    //Concatenate all elements of resultList into one string
-    resultEdit->setText(resultList.join("\n"));
-    resultWindow->show();
+    //resultWindow->show();
 }
--- Task_1(Qt)/json_parse/parse.h
+++ Task_1(Qt)/json_parse/parse.h
@@ -1,9 +1,44 @@
 #ifndef PARSE_H
 #define PARSE_H
 
-#include <QWidget>
 #include <QTextEdit>
 #include <QPushButton>
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QString>
+
+class ResultWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ResultWindow(QWidget *parent = nullptr,QString str="");
+    void Parse(QString str);
+
+private:
+    QLabel* firstName;
+    QLabel* secondName;
+    QLabel* age;
+    QLabel* fathersName;
+    QLabel* fathersAge;
+    QLabel* fathersWeight;
+    QLabel* mothersName;
+    QLabel* mothersAge;
+    QLabel* mothersWeight;
+
+    QLabel* firstNamea;
+    QLabel* secondNamea;
+    QLabel* agea;
+    QLabel* fathersNamea;
+    QLabel* fathersAgea;
+    QLabel* fathersWeighta;
+    QLabel* mothersNamea;
+    QLabel* mothersAgea;
+    QLabel* mothersWeighta;
+};
+
+
 
 class MainWindow : public QWidget
 {
@@ -15,10 +50,14 @@ public:
 private slots:
     void parseJson();
 
+
 private:
     QTextEdit* jsonEdit;
     QPushButton* parseButton;
 };
 
-
 #endif
+
+
+
+
